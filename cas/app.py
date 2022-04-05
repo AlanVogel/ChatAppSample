0f9277d90fd570f78268d527668e4d20d79cde8d
Alan Vogel <alan.vogel@axxessio.com>
2022-04-05 15:26:54 +0200

    small fixes

--- cas/app.py
+++ cas/app.py
@@ -8,12 +8,12 @@ from flask import (
 from flask_sqlalchemy import SQLAlchemy
 from cas.database import (
     recreate_database,
-    session,
     User,
     Message,
     Conversation,
     ConversationUser,
     ConversationMessage,
+    Session,
 )
 from cas.utils import (
     email_validator,
@@ -31,10 +31,12 @@ recreate_database()
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    if email_validator(data['email']):
-        session.add(User(nick_name=data['nick_name'], email=data['email'],
-                         key_word=data['key_word']))
-        session.commit()
+    if email_validator(data.get('email')):
+        with Session.begin() as session:
+            session.add(
+                User(nick_name=data.get('nick_name'), email=data.get('email'),
+                     key_word=data.get('key_word')))
+            session.commit()
         return jsonify({'success': 'User created!'}), 200
     return jsonify({'error': 'wrong email, try again!'}), 400
 
@@ -42,9 +44,12 @@ def register():
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    user = session.query(User).filter(User.email == data['email']).first()
-    if user.email == data['email']:
-        token = jwt.encode({'user': user.nick_name,
+    with Session.begin() as session:
+        user = session.query(User).filter(
+            User.email == data.get('email')).first()
+        user_name = user.nick_name
+    if user_name:
+        token = jwt.encode({'user': user_name,
                             'exp': datetime.datetime.utcnow() + \
                                    datetime.timedelta(
                                        minutes=15)}, app.config['SECRET_KEY'])
@@ -54,35 +59,44 @@ def login():
                                                     algorithms=header_data[
                                                         'alg']),
                         'token': token}), 200
-    return jsonify({'error': 'could not verify!'}), 400
+    return jsonify({'error': 'user does no exist!'}), 400
 
 
 @app.route('/chat', methods=['POST'])
 @token_required
 def chat():
     data = request.get_json()
-    user = session.query(User).filter(
-        User.nick_name == data['nick_name']).first()
     date = datetime.datetime.utcnow()
-    session.add(Message(msg=data['msg'], created_at=date,
-                        sender_id=user.id))
-    session.add(Conversation(conversation_name=data['conversation_name']))
-    conversation_user = session.query(Conversation).filter(
-        Conversation.conversation_name == data['conversation_name']).first()
-    session.add(ConversationUser(user_id=user.id,
-                                 conversation_id=conversation_user.id))
-    message = session.query(Message).filter(Message.msg == data['msg']).first()
-    session.add(ConversationMessage(conversation_id=conversation_user.id,
-                                    message_id=message.id))
-    session.commit()
+    with Session.begin() as session:
+        user = session.query(User).filter(
+            User.nick_name == data.get('nick_name')).first()
+        session.add(Message(msg=data.get('msg'), created_at=date,
+                            sender_id=user.id))
+        session.add(
+            Conversation(conversation_name=data.get('conversation_name')))
+        conversation_user = session.query(Conversation).filter(
+            Conversation.conversation_name == data.get(
+                'conversation_name')).first()
+        session.add(ConversationUser(user_id=user.id,
+                                     conversation_id=conversation_user.id))
+        message = session.query(Message).filter(
+            Message.msg == data.get('msg')).first()
+        session.add(ConversationMessage(conversation_id=conversation_user.id,
+                                        message_id=message.id))
+        session.commit()
     return jsonify({'success': 'Chat created!'}), 200
 
 
-@app.route('/user/<id>', methods=['GET'])
+@app.route('/user/<int:id>', methods=['GET'])
 def get_user(id):
-    user = session.query(User).filter(User.id == id).first()
-    del user.__dict__['_sa_instance_state']
-    return jsonify(user.__dict__), 200
+    with Session.begin() as session:
+        user = session.query(User).filter(User.id == id).first()
+    # del user.__dict__['_sa_instance_state']
+    # return jsonify(user.__dict__), 200
+    return jsonify({'id': user.id,
+                    'nick_name': user.nick_name,
+                    'email': user.email,
+                    'key_word': user.key_word})
 
 
 if __name__ == '__main__':
