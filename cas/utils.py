19959f562cfeeb9a213eabd1c764aa0eaa6772ab
Alan Vogel <alan.vogel@axxessio.com>
2022-04-08 19:07:51 +0200

    [feature/jwt_token] add ok/error responses, encode/decode jwt token and random string of a key_word

--- cas/utils.py
+++ cas/utils.py
@@ -1,5 +1,8 @@
-import re
 import jwt
+import json
+import random
+import string
+from datetime import datetime
 from flask import (
     Flask,
     request,
@@ -10,12 +13,79 @@ from functools import wraps
 app = Flask(__name__)
 
 
-def email_validator(email):
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    if re.fullmatch(regex, email):
-        return True
-    else:
-        return False
+def now() -> datetime:
+    return datetime.now()
+
+
+def random_string(size):
+    return ''.join(
+        [random.choice(string.ascii_letters + string.digits) for n in
+         range(size)])
+
+
+def encode_security_token(user_id: int, nick_name: str, key_word: str):
+    """
+    The function will encode the security token.
+    :param user_id:
+    :param nick_name:
+    :param key_word:
+    :return: False or hash (str)
+    """
+    return jwt.encode(
+        {"user_id": user_id,
+         "nick_name": nick_name,
+         }, key_word, algorithm='HS256')
+
+
+def decode_security_token(token: str, key_word: str):
+    """
+    The function will decode the security token.
+    :param token:
+    :param key_word:
+    :return: False or user data (dict)
+    """
+    try:
+        return jwt.decode(token, key_word, algorithms='HS256')
+    except jwt.ExpiredSignatureError:
+        return jwt.ExpiredSignatureError("Signature has expired.")
+    except Exception as ex:
+        return Exception(f"Something is wrong with security-token. {str(ex)}")
+
+
+def ok_response(message, status_code=200, **additional_data):
+    """
+    The function will create https ok response
+    :param message:
+    :param status_code:
+    :param additional_data:
+    :return: response object: dict
+    """
+    data = {
+        'status': 'OK',
+        'code': status_code,
+        'server_time': now().strftime("%Y-%m-%dT%H:%M:%S"),
+        'message': message,
+    }
+    for k, v in additional_data.items():
+        data['{0}'.format(k)] = v
+    return jsonify({"info": {'data': data}, "status_code": status_code})
+
+
+def error_response(message, status_code):
+    """
+    The function will create https error response
+    :param message:
+    :param status_code:
+    :return: response object: dict
+    """
+    data = {
+        'status': 'ERROR',
+        'code': status_code,
+        'server_time': now().strftime("%Y-%m-%dT%H:%M:%S"),
+        'message': message
+    }
+
+    return jsonify({"info": {'data': data}, "status_code": status_code})
 
 
 def token_required(f):
